14ae81f08212de52cfe61da37e04059096a542e9
Chrismessiah <chrabd@kth.se>
2015-12-02 15:51:46 +0100

    Slängde ihop en heder och foster

---
+++ Projektet/Webbsida/footer.php
@@ -0,0 +1,43 @@
+<?php
+	$footer_text = "Labore admodum quo ea. Prima rationibus sit at, eam iudico veniam eloquentiam cu. Putent cetero electram et eam, mea cu integre aliquando vulputate. Te ius alia soluta patrioque, ad debet paulo vituperata per, nostrud utroque voluptua mei ne.Mel nullam bonorum ad, quo utamur patrioque scribentur an. Vel et facer apeirian interesset, id vis porro vulputate, est nemore persius inciderint ei. Eu graeci dictas vis. In nostro praesent sea, ei quas disputando suscipiantur sea, mei choro dolor dissentiet ad.";
+?>
+
+<div id="foot_div">
+	<div id="foot_textbox">
+		<p id="foot_p"><?=$footer_text?></p>
+	</div>
+	<div id="foot_chris"></div>
+	<div id="foot_adde"></div>
+</div>
+
+<style type="text/css">
+	#foot_div {
+		background: #353535;
+	}
+	#foot_textbox {
+		display: inline-block;
+		width: 730px;
+		height: 246px;
+	}
+
+	#foot_chris, #foot_adde {
+		margin-top: 10px;
+		display: inline-block;
+		width: 246px;
+		height: 246px;
+		position: relative;
+		left: 5%
+		
+	}
+	
+	#foot_chris {background-image: url("public/footer/chris.png");}
+	#foot_adde {background-image: url("public/footer/adde.png");}
+	
+	#foot_p {
+		font-family: Helvetica;
+		font-size: 24px;
+		color: #FFFFFF;
+		line-height: 29px;
+		margin: 5px;
+	}
+</style>
---
+++ Projektet/Webbsida/header.php
@@ -0,0 +1,70 @@
+<?php ?>
+<style>
+	body {
+		margin: 0px;
+	}
+</style>
+
+<div id="head_div">
+	<a href="index.php">
+		<div id="kth_logo"></div>
+	</a>
+
+	<?php 
+		// Set to false just to get something
+		$logged_in = FALSE;
+		if ($logged_in) {
+	?>
+			<p class="head_menu">Kurser</p>
+			<p class="head_menu">Schema</p>
+			<p class="head_menu">Resultat</p>
+			<p class="head_menu">Översikt</p>
+			<p class="head_menu">Övrigt</p>
+
+			<style>
+				.head_menu {
+					display: inline-block;
+					font-family: Helvetica;
+					font-size: 24px;
+					color: #FFFFFF;
+					line-height: 29px;
+					position: relative;
+				} 
+			</style>
+	
+	<?php } else { ?>
+	
+	<p class="head_menu">Log in</p>
+	<style>
+		.head_menu {
+			display: inline-block;
+			font-family: Helvetica;
+			font-size: 24px;
+			color: #FFFFFF;
+			line-height: 29px;
+			position: relative;
+			left: 80%;
+			display: inline-block;
+		}
+	</style>
+	<?php } ?>
+
+</div>
+
+
+<style>
+	#kth_logo {
+		background-image: url("public/header/kth.png");
+		width: 60px;
+		height: 60px;
+		display: inline-block;
+		position: relative;
+		left: 15px;
+		top: 15px;
+	}
+	#head_div {
+		background: #525259;
+	}
+</style>
+
+
