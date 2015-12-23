ba80cc6cc931a756e3868a714001d36b55ce6059
Chrismessiah <chrabd@kth.se>
2015-12-23 20:54:58 +0100

    lite css fix

--- Projektet/Webbsida/login_page.php
+++ Projektet/Webbsida/login_page.php
@@ -7,11 +7,13 @@
 	
 	<div id="login_wrapper">
 		<form method="post" action="login.php"> <!-- collects user input -->
-			<input type="text" name="field1" placeholder="Username"/>
+			<div id="input_boxes">
+				<input type="text" name="field1" placeholder="Username"/>
+				<input type="password" name="field2" placeholder="Password"/>
+				<a href="login_page.php?error=forgot_pwd" id="login_pass_forgot">Forgot password?</a>
+				<div><input type="submit" value="Login"/></div>
+			</div>
 
-			<input type="password" name="field2" placeholder="Password"/>
-			<a href="login_page.php?error=forgot_pwd"><p id="login_pass_forgot">Forgot password?</p></a>
-			<input type="submit" value="Login"/>
 		</form>
 	</div>
 
@@ -19,17 +21,32 @@
 
 
 <style>
+	#input_boxes {
+		width: 60%;
+		margin: auto;
+	}
+
+	#input_boxes > input {
+		margin: 10px; 
+	}
+	#input_boxes > div {
+		width: 50%;
+		margin: auto; 
+	}
+
 	#login_wrapper {
 		position: relative;
 		margin: auto;
 	    width: 32%;
-	    top: 400px;
+	    top: 420px;
 	}
 	#login_pass_forgot {
 		color: #FFFFFF;
 		font-family: Helvetica;
 		position: relative;
-		left: 280px;
+		left: 110px;
+		font-size: 12px;
+		margin: 0px;
 	}
 	input[type="submit"]:hover {
 		cursor: pointer; 
@@ -37,8 +54,8 @@
 	}
 	#login-bg {
 		background-image: url("public/Albert-Einstein-genius-copy.jpg");
-		height: 800px;
-		background-size: 1294px;
+		height: 750px;
+		background-size: 100%;
 		background-repeat: no-repeat;
 	}
 	input[type="text"], input[type="password"] {
@@ -64,7 +81,8 @@
 		padding-left: 30px;
 		padding-right: 30px;
 		position: relative;
-		left: 150px;
+		top: 40px;
+		
 	}
 	a {
 		text-decoration: none;
