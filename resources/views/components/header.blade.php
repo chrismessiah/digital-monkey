0a417398e08c505e2040c439a8e7e3fcfad305c7
Christian Abdelmassih <chrabd@kth.se>
2016-07-11 11:39:36 +0200

    add dropdown menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,10 +4,15 @@
 		@if (Auth::guest()) {{-- Not logged in --}}
 			<a href="/register">Register</a>
 			<a href="/login">Log in</a>
-
 		@else {{-- logged in --}}
-			<a href="">Control Panel</a>
-			<a href="{{ url('/logout') }}">Logout</a>
+			<div class="dropdown">
+				<div class="title">username</div>
+				<div class="content">
+					<a href="">Post</a>
+					<a href="">Preferences</a>
+					<a href="{{ url('/logout') }}">Logout</a>
+				</div>
+			</div>
 		@endif
 		
 		{{-- <a href=""><p id="head_menu4">Change to Swedish</p></a> --}}
