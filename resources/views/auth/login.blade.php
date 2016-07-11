73d2244e3fc470dfb7950fedb0696bb6e4cde509
Christian Abdelmassih <chrabd@kth.se>
2016-07-11 18:05:59 +0200

    Add login with facebook button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,12 +9,13 @@
             <input id="email" type="email" name="email" placeholder="email" value="{{ old('email') }}">
             <input id="password" type="password" name="password" placeholder="password">
 
-            <p>Remember me?</p><input type="checkbox" name="remember">
+            {{-- <p>Remember me?</p><input type="checkbox" name="remember"> --}}
+            {{-- <a href="{{ url('/password/reset') }}">Forgot Password?</a> --}}
 
-            <div class="submit-container">
-                <button type="submit" class="btn login">Login</button>
-                <a class="btn reset" href="{{ url('/password/reset') }}">Reset Password?</a>
-            </div>
+            <button type="submit" class="btn btn-login">Login with email</button>
+            <a href="{{ url('/auth/facebook') }}" class="btn btn-facebook">Login with facebook</a>
+            
+            
         </form>
     </div>
 </div>
