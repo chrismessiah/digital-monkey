519a2bc71cd8d946a0ff48c44ff4e2d0946d9519
Christian Abdelmassih <chrabd@kth.se>
2016-07-12 23:04:24 +0200

    old body in /create

--- resources/views/blogpost/write.blade.php
+++ resources/views/blogpost/write.blade.php
@@ -6,7 +6,7 @@
           {{ csrf_field() }}
           <input id="title" placeholder="title" type="text" name="title" value="{{ old('title') }}">
           <input id="intro" placeholder="intro" type="text" name="intro" value="{{ old('intro') }}">
-          <textarea id="body" placeholder="body" name="body" rows="8" cols="40"></textarea>
+          <textarea id="body" placeholder="body" name="body" rows="8" cols="40">{{ old('body') }}</textarea>
           <input id="file" type="file" name="file">
           <button type="submit" class="btn">Post</button>                
       </form>
