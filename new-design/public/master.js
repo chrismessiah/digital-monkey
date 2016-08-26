437f0b8010cc6ce6bbc9df3e955978d79dcbbdc8
Christian Abdelmassih <chrabd@kth.se>
2016-08-26 12:56:21 +0200

    login-window for header

--- new-design/public/master.js
+++ new-design/public/master.js
@@ -22,3 +22,40 @@ $(document).ready(function() {
   }
   
 });
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+$(document).ready(function() {
+  
+  $("#header").find('.standard').find('.login').click(function() {
+    $($("body").children()[1]).css({'-webkit-filter': 'blur(5px)'});
+    $("#header").css({
+      'height':'100vh',
+      'position': 'fixed',
+      'background-color': 'rgba(28, 34, 40, 0.96)',
+    });
+    $("#header").find('.standard').css({'display':'none'});
+    $("#header").find('.login-dropdown').css({'display':'flex'});
+    window.onwheel = preventDefault;
+    window.ontouchmove  = preventDefault;
+  });
+  
+  $("#header").find('.login-dropdown').find('.back').click(function() {
+    $($("body").children()[1]).css({'-webkit-filter': 'none'});
+    $("#header").css({
+      'height':'60px',
+      'position': 'static',
+      'background-color': '#1c2227',
+    });
+    $("#header").find('.standard').css({'display':'block'});
+    $("#header").find('.login-dropdown').css({'display':'none'});
+    window.onwheel = null;
+    window.ontouchmove  = null;
+  });
+  
+});
