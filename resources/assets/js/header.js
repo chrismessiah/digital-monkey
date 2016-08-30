1e3fd50c62a0a5ffa4b15061baebb12807025b7d
Christian Abdelmassih <chrabd@kth.se>
2016-08-30 22:45:30 +0200

    updated header for register

--- resources/assets/js/header.js
+++ resources/assets/js/header.js
@@ -5,27 +5,46 @@ function preventDefault(e) {
   e.returnValue = false;  
 }
 
+var showingDropdown = false;
+
 $(document).ready(function() {
   
   // show dropdown
   $("#header").find('.standard').find('.login').click(function() {
-    $($("body").children()[1]).css({'-webkit-filter': 'blur(5px)'});
+    $($("body").children()[1]).css({
+      '-webkit-filter': 'blur(5px)',
+      'position':'fixed'
+    });
     $("#header").css({
-      'height':'100vh',
-      'position': 'fixed',
+      'min-height':'100vh',
+      'height':'auto',
+      'position': 'absolute',
       'background-color': 'rgba(28, 34, 40, 0.96)',
     });
     $("#header").find('.standard').css({'display':'none'});
     $("#header").find('.login-dropdown').css({'display':'flex'});
-    window.onwheel = preventDefault;
-    window.ontouchmove  = preventDefault;
+    //window.onwheel = preventDefault;
+    //window.ontouchmove  = preventDefault;
   });
+  
+  // if (showingDropdown) {
+  //   $(window).scroll(function(e) {
+  //     var height = $("#header").height();
+  //     if($(window).scrollTop() >= height) {
+  //        $(window).scrollTop(height);
+  //     }  
+  //   });
+  // }
   
   // hide dropdown
   $("#header").find('.login-dropdown').find('.back').click(function() {
-    $($("body").children()[1]).css({'-webkit-filter': 'none'});
+    $($("body").children()[1]).css({
+      '-webkit-filter': 'none',
+      'position':'initial'
+    });
     $("#header").css({
       'height':'60px',
+      'min-height':'0',
       'position': 'static',
       'background-color': '#1c2227',
     });
@@ -33,6 +52,23 @@ $(document).ready(function() {
     $("#header").find('.login-dropdown').css({'display':'none'});
     window.onwheel = null;
     window.ontouchmove  = null;
+    showingDropdown = false;
+  });
+  
+  // go to sign-up
+  $("#header").find('.login-dropdown').find('.sign-in').find('.redirect').click(function() {
+    $("#header").find('.login-dropdown').find('.sign-in').css({'display':'none'});
+    $("#header").find('.login-dropdown').find('.sign-in').animate({'opacity':"0"});
+    $("#header").find('.login-dropdown').find('.sign-up').css({'display':'flex'});
+    $("#header").find('.login-dropdown').find('.sign-up').animate({'opacity':"1"});
+  });
+  
+  // go to sign-in
+  $("#header").find('.login-dropdown').find('.sign-up').find('.redirect').click(function() {
+    $("#header").find('.login-dropdown').find('.sign-in').css({'display':'flex'});
+    $("#header").find('.login-dropdown').find('.sign-in').animate({'opacity':"1"});
+    $("#header").find('.login-dropdown').find('.sign-up').css({'display':'none'});
+    $("#header").find('.login-dropdown').find('.sign-up').animate({'opacity':"0"});
   });
   
   // show dropdown (when logged in)
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -35,20 +35,41 @@
 		<div class="login-dropdown">
 				<div class="back"></div>
 				<div class="logo"></div>
-				<p>Sign in / Sign up</p>
-				<div class="container">
-						<form role="form" method="POST" action="{{ Helper::env_url('/login') }}">
-								{{ csrf_field() }}
-								<input id="email" type="email" name="email" placeholder="email" value="{{ old('email') }}">
-								<input id="password" type="password" name="password" placeholder="password">
+				
+				<div class="sign-in">	
+					<p class="title">Sign in</p>
+					<div class="container">
+							<a href="{{ Helper::env_url('/auth/facebook') }}" class="btn-facebook">Login with facebook</a>
+							<p class="separator">or</p>
+							<form role="form" method="POST" action="{{ Helper::env_url('/login') }}">
+									{{ csrf_field() }}
+									<input id="email" type="email" name="email" placeholder="email" value="{{ old('email') }}">
+									<input id="password" type="password" name="password" placeholder="password">
 
-								{{-- <p>Remember me?</p><input type="checkbox" name="remember"> --}}
-								{{-- <a href="{{ Helper::env_url('/password/reset') }}">Forgot Password?</a> --}}
-								<button type="submit" class="btn-light">Login with email</button>
-								<p class="separator">or</p>
-								<a href="{{ Helper::env_url('/auth/facebook') }}" class="btn-facebook">Login with facebook</a>							
-						</form>
+									{{-- <p>Remember me?</p><input type="checkbox" name="remember"> --}}
+									{{-- <a href="{{ Helper::env_url('/password/reset') }}">Forgot Password?</a> --}}
+									<button type="submit" class="btn-light">Login with email</button>
+							</form>
+							<p class="redirect">Don't have an account yet?</p>
+					</div>
 				</div>
-				<a href="{{ Helper::env_url('/register') }}">Don't have an account yet?</a>
+				
+				<div class="sign-up">	
+					<p class="title">Sign up</p>
+					<div class="container">
+							<a href="{{ Helper::env_url('/auth/facebook') }}" class="btn-facebook">Sign up with facebook</a>
+							<p class="separator">or sign up with email</p>
+							<form role="form" method="POST" action="{{ Helper::env_url('/register') }}">
+									{{ csrf_field() }}
+									<input id="firstname" placeholder="firstname" type="text" name="firstname" value="{{ old('firstname') }}">
+									<input id="lastname" placeholder="lastname" type="text" name="lastname" value="{{ old('lastname') }}">
+									<input id="email" placeholder="email" type="email" name="email" value="{{ old('email') }}">
+									<input id="password" placeholder="password" type="password" name="password">
+									<input id="password-confirm" placeholder="confirm password" type="password" name="password_confirmation">
+									<button type="submit" class="btn-light">Sign up</button>    
+							</form>
+							<p class="redirect">Wait! I do have an account!<p>
+					</div>
+				</div>
 		</div>
 </div>
