6c234b4abcbcafeedbaed916dbea39a1da23ffb2
Christian Abdelmassih <chrabd@kth.se>
2016-10-15 09:39:48 +0200

    various updates

--- resources/assets/js/unimplemented-functions.js
+++ resources/assets/js/unimplemented-functions.js
@@ -1,14 +1,23 @@
+function functionNotImplemented() {
+  swal({
+    title: 'Nope',
+    text: 'This functionallity hasn\'t been implemented yet. Sorry about that.',
+    type: 'info',
+    showConfirmButton: false,
+    timer: '3000'
+  });
+}
+
 $(document).ready(function() {
-  var categoryBoxes = $('#index').find('.categories').find('div');
-  if ($('#index').length > 0) {
-    categoryBoxes.click(function() {
-      swal({
-        title: 'Nope',
-        text: 'This functionallity hasn\'t been implemented yet. Sorry about that.',
-        type: 'info',
-        showConfirmButton: false,
-        timer: '3000'
-      });
-    });
+  var searchBox = $('#header').find('.standard').find('.right').find('.search');
+  if ($('#header').find('.login-dropdown').length > 0) {
+    var forgotPass = $('#header').find('.login-dropdown').find('.sign-in').find('.container').find('.pass-forgot');
+    forgotPass.click(function() {functionNotImplemented();});
   }
+  $('#header').find('.login-dropdown').find('.sign-in').find('.container').find('.pass-forgot')
+  if ($('#index').length > 0) {
+    var categoryBoxes = $('#index').find('.categories').find('div');
+    categoryBoxes.click(function() {functionNotImplemented();});
+  }
+  searchBox.click(function() {functionNotImplemented();});
 });
