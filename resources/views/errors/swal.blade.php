b315804482f089e8352d64cbaa981b03cf43a100
Christian Abdelmassih <chrabd@kth.se>
2016-10-17 16:45:04 +0200

    Better error logging

--- resources/views/errors/swal.blade.php
+++ resources/views/errors/swal.blade.php
@@ -1,12 +1,18 @@
 @if (count($errors) > 0)
-  <script type="text/javascript">
-    swal("Errors have occured!");
-  </script>
-    <ul>
-      @foreach ($errors->all() as $error)
-       <li>{{ $error }}</li>
-      @endforeach
-    </ul>
+  <?php
+  $array = $errors->all();
+  $string = 'The following errors have occured \n';
+  for ($i=0; $i < sizeof($array); $i++) { 
+    $string = $string.'\n   '.($i+1).'.  '.$array[$i];
+  }
+  ?>
+    <script type="text/javascript">
+      swal({
+        title: "Oh no!",
+        text: "<?php echo $string ?>",
+        type: "warning"
+      });
+    </script>
 @elseif(isset($error))
   <script type="text/javascript">
     swal({{$error}});
