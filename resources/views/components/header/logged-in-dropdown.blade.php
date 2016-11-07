06d23f0ccb03ce30c39d4573d7d0b04856fdb8bd
Christian Abdelmassih <chrabd@kth.se>
2016-11-07 10:14:59 +0100

    add card token in header form

--- resources/views/components/header/logged-in-dropdown.blade.php
+++ resources/views/components/header/logged-in-dropdown.blade.php
@@ -2,6 +2,7 @@
   <div class="triangle"></div>
   <a href="{{Helper::env_url('/blogposts/write')}}">Make blogpost</a>
   <form action="{{ Helper::env_url('/logout') }}" method="POST">
+    {{ csrf_field() }}
     <button type="submit">Log out</button>
   </form>
   {{-- <a href="{{Helper::env_url('/logout')}}">Log out</a> --}}
